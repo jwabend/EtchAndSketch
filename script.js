9be40d8619ae015bbb1954d52f3bcf34064aff28
jwabend <63935497+jwabend@users.noreply.github.com>
2024-07-16 13:56:07 -0400

    Update script.js
    fixed the mouse over function so that the listener includes columns. without line 6, the column border disapears.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gridContainer = document.getElementById("container");
+    const gridSizeButton = document.querySelector("button[type='grid-size']");
 
-    gridContainer.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "black";
-        event.target.style.borderColor = "white";
+    document.addEventListener("mouseover", function(event) {
+        if (event.target.classList.contains("column")) {
+            event.target.style.backgroundColor = "black";
+            event.target.style.borderColor = "white";
+        }
     });
 
-    // element.addEventListener("click", function() {
-    //     alert("test");
-    // });
+   
 
-    const gridSizeButton = document.querySelector("button[type='grid-size']");
+    
     gridSizeButton.addEventListener("click", function() {
         const container = document.getElementById('container');
         container.innerHTML = ''; // Clear existing grid
@@ -24,20 +25,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-function makeGrid(rowCount, colCount) {
+function makeGrid(rowCount,columnCount) {
     const container = document.getElementById('container');
 
     for (let r = 0; r < rowCount; r++) {
         let row = document.createElement("div");
         row.classList.add("row");
-        row.setAttribute("id", "row-id");
         container.appendChild(row);
 
-        for (let c = 0; c < colCount; c++) {
+        for (let c = 0; c < columnCount; c++) {
             let column = document.createElement("div");
             column.classList.add("column");
-            column.setAttribute("id", "column-id");
             row.appendChild(column);
         }
     }
 }
+
+makeGrid(16,16)
