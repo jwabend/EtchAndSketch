341f1e1e196611ef7ba530b85d0eb4fae45a21d1
jwabend <63935497+jwabend@users.noreply.github.com>
2024-07-16 12:40:46 -0400

    update script.js
    removed grid by setting innerHTML of container. prompt user for row and column to determine grid size

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
         event.target.style.backgroundColor = "black";
         event.target.style.borderColor = "white";
     });
+
+    // element.addEventListener("click", function() {
+    //     alert("test");
+    // });
+
+    const gridSizeButton = document.querySelector("button[type='grid-size']");
+    gridSizeButton.addEventListener("click", function() {
+        const container = document.getElementById('container');
+        container.innerHTML = ''; // Clear existing grid
+
+        let rowCount = prompt("Enter row number");
+        let columnCount = prompt("Enter column number");
+
+        makeGrid(rowCount,columnCount)
+    });
 });
+
+
 
 function makeGrid(rowCount, colCount) {
     const container = document.getElementById('container');
@@ -13,13 +30,14 @@ function makeGrid(rowCount, colCount) {
     for (let r = 0; r < rowCount; r++) {
         let row = document.createElement("div");
         row.classList.add("row");
+        row.setAttribute("id", "row-id");
         container.appendChild(row);
 
         for (let c = 0; c < colCount; c++) {
             let column = document.createElement("div");
             column.classList.add("column");
+            column.setAttribute("id", "column-id");
             row.appendChild(column);
         }
     }
 }
-makeGrid(16,16)
